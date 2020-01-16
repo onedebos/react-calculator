73ec0c4ebd4daeff7831db4df30e19c5745b2fd1
Adebola <onedebos@gmail.com>
2020-01-16 12:17:12 +0100

    style calculator app

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Button.css';
+import './styles/Button.css';
 
 const Button = (props) => {
   const { buttonName } = props;
-  return (
+  const { color } = props;
+  const { wide } = props;
+
+  return wide ? (
     <div className="button-component" style={{ width: '50%' }}>
-      <button type="button">{buttonName}</button>
+      <button type="button" style={{ backgroundColor: color }}>
+        {buttonName}
+      </button>
+    </div>
+  ) : (
+    <div className="button-component" style={{ width: '25%' }}>
+      <button style={{ backgroundColor: color }} type="button">
+        {buttonName}
+      </button>
     </div>
   );
 };
 Button.propTypes = {
   buttonName: PropTypes.string,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
 };
 
 Button.defaultProps = {
   buttonName: '',
+  color: '#fc8f2f',
+  wide: false,
 };
 
 export default Button;
--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -1,35 +1,35 @@
 import React from 'react';
 import Button from './Button';
-import './ButtonPanel.css';
+import './styles/ButtonPanel.css';
 
 const ButtonPanel = () => (
   <div className="button-panel-component">
-    <div className="gr-1">
+    <div className="row">
       <Button buttonName="AC" />
       <Button buttonName="+/-" />
       <Button buttonName="%" />
       <Button buttonName="/" />
     </div>
-    <div className="gr-2">
+    <div className="row">
       <Button buttonName="7" />
       <Button buttonName="8" />
       <Button buttonName="9" />
       <Button buttonName="X" />
     </div>
-    <div className="gr-3">
+    <div className="row">
       <Button buttonName="4" />
       <Button buttonName="5" />
       <Button buttonName="6" />
       <Button buttonName="-" />
     </div>
-    <div className="gr-4">
+    <div className="row">
       <Button buttonName="1" />
       <Button buttonName="2" />
       <Button buttonName="3" />
       <Button buttonName="+" />
     </div>
-    <div className="gr-5">
-      <Button buttonName="0" />
+    <div className="row gr-5">
+      <Button className="zero" buttonName="0" />
       <Button buttonName="." />
       <Button buttonName="=" />
     </div>
