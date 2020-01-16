1abf6c91ea6c9f20d75c2200df9ba80785534fb6
Adebola <onedebos@gmail.com>
2020-01-16 18:59:41 +0100

    fix tse changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,25 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './styles/Button.css';
 
-const Button = (props) => {
-  const { buttonName } = props;
-  const { color } = props;
-  const { wide } = props;
-
-  return wide ? (
-    <div className="button-component" style={{ width: '50%' }}>
-      <button type="button" style={{ backgroundColor: color }}>
-        {buttonName}
-      </button>
-    </div>
-  ) : (
-    <div className="button-component" style={{ width: '25%' }}>
-      <button style={{ backgroundColor: color }} type="button">
-        {buttonName}
-      </button>
-    </div>
-  );
-};
+const Button = ({ buttonName, color, wide }) => (
+  <div className="button-component" style={{ width: wide ? '50%' : '25%' }}>
+    <button type="button" style={{ backgroundColor: color }}>
+      {buttonName}
+    </button>
+  </div>
+);
 Button.propTypes = {
   buttonName: PropTypes.string,
   color: PropTypes.string,
